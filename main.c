8fd5da58163e9a0b54b4916f132905c06b305304
Diego Leiva <di3go21@gmail.com>
2020-04-09 15:34:03 +0200

    comentario en main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// ESTO ES EL COMENTARIO QUE VAMOS A SUBIR
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
